49720fb5d4bb56f8ee6d52271f6267dc5bd1ce7f
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-01-26 10:21:06 +0100

    modif Manche

--- src/main/java/org/centrale/tp_note/Manche.java
+++ src/main/java/org/centrale/tp_note/Manche.java
@@ -5,7 +5,9 @@
 package org.centrale.tp_note;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +18,8 @@ public class Manche {
     private String codeur;
     private String decodeur;
     private Ligne code;
-    private List<Ligne> jeu;
+    private List<String> jeu;
+    private static int TOURMAX = 12;
     
     public Manche(String codeur, String decodeur){
         this.codeur = codeur;
@@ -24,5 +27,43 @@ public class Manche {
         this.code = new Ligne();
         this.jeu = new ArrayList<>();
     }
+    
+    public void init(Scanner scan){
+        boolean valide = false;
+        while (!valide){
+            System.out.println(codeur + ", à vous de choisir un code.");
+            String str = scan.nextLine();
+            List<String> liste= new ArrayList<String>(Arrays.asList(str.split("")));
+            valide = code.verifLigneJeu(liste);
+            if (valide){
+                code.setLigneJeu(liste);
+            }
+        }
+    }
+    
+    public int Jeu(){
+        Scanner scan = new Scanner(System.in);
+        init(scan);
+        int tour = 0;
+        boolean finJeu = false;
+        while(tour<TOURMAX && !finJeu){
+            Ligne ligne = new Ligne();
+            int[] resultat;
+            boolean valide = false;
+            while (!valide){
+                System.out.println("Essai " + (tour+1) );
+                String str = scan.nextLine();
+                List<String> liste= new ArrayList<String>(Arrays.asList(str.split("")));
+                valide = ligne.verifLigneJeu(liste);
+                if (valide){
+                    ligne.setLigneJeu(liste);
+                }
+            }
+            resultat = this.code.compareListsAndCount(ligne);
+            this.jeu.add(ligne)
+            
+            
+        }
+    } 
     
 }
