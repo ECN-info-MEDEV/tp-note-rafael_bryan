8a1515736028f318fab50a8fec406b69038d91b0
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-01-26 08:55:57 +0100

    Création Partie

---
+++ src/main/java/org/centrale/tp_note/Manche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.tp_note;
+
+/**
+ *
+ * @author bryan & rafael
+ */
+public class Manche {
+    
+    private String codeur;
+    private String decodeur;
+    private Ligne code;
+    private Ligne[] jeu;
+}
