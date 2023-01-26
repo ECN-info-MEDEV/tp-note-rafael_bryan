0b6cfc3111a5b061f5b917549a75c416fb8184fa
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-01-26 11:26:25 +0100

    Ajout déroulement du jeu dans partie

--- src/main/java/org/centrale/tp_note/Partie.java
+++ src/main/java/org/centrale/tp_note/Partie.java
@@ -11,10 +11,7 @@ import java.util.Scanner;
  * @author bryan & rafael
  */
 public class Partie {
-    /**
-     * The current turn of the game.
-     */
-    private int tour;
+
     /**
      * The first player in the game.
      */
@@ -28,7 +25,6 @@ public class Partie {
      * Constructs a new Partie object, asking for the names of the two players and creating new Joueur objects for them.
      */
     public Partie(){
-        this.tour = 0;
         System.out.println("Nom Joueur 1 :");
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
@@ -41,9 +37,47 @@ public class Partie {
     /**
      * Runs the game.
      *
-     * @return true if the game is still in progress, false otherwise
      */
-    public boolean jeu(){
-        return true;
+    public void jeu(){
+        boolean enJeu = true;
+        int score;
+        Manche manche;
+        while (enJeu){
+            manche = new Manche(j1.getNom(),j2.getNom());
+            score = manche.Jeu();
+            j1.ajoutPoint(score);
+            manche = new Manche(j2.getNom(),j1.getNom());
+            score = manche.Jeu();
+            j2.ajoutPoint(score);
+            
+            //affichage des scores
+            System.out.println("Scores actuels");
+            System.out.println(j1.getNom() + " a " + j1.getPoints() + " points.");
+            System.out.println(j2.getNom() + " a " + j2.getPoints() + " points.");
+            
+            // Continuation du jeu
+            boolean choisi = false;
+            while (!choisi){
+                System.out.println("Voulez-vous continuer ? (y/n)");
+                Scanner scan = new Scanner(System.in);
+                String str = scan.nextLine();
+                if (str.equals("y")){
+                    choisi = true;
+                } else if (str.equals("n")){
+                    choisi = true;
+                    enJeu = false;
+                }
+            }
+        }
+        
+        //affichage du résultat
+        System.out.println("Résultat : ...");
+        if (j1.getPoints()>j2.getPoints()){
+            System.out.println(j1.getNom() + " a gagné !");
+        } else if (j2.getPoints()>j1.getPoints()){
+            System.out.println(j2.getNom() + " a gagné !");
+        } else {
+            System.out.println("Égalité !!");
+        }
     }
 }
