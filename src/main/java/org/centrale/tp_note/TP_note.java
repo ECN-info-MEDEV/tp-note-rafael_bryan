e3af421641e66048b9066877ed5ce6a4907f67f7
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-01-26 11:36:05 +0100

    rien a commit

--- src/main/java/org/centrale/tp_note/TP_note.java
+++ src/main/java/org/centrale/tp_note/TP_note.java
@@ -7,12 +7,13 @@ package org.centrale.tp_note;
 
 /**
  *
- * @author bryan
+ * @author bryan & rafael
  */
 public class TP_note {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!2");
+        Partie partie = new Partie();
+        partie.jeu();
     }
 }
